869169506bc78a3f50254c9e5832f004c9c464a5
MelinaCruzF <78770217+MelinaCruzF@users.noreply.github.com>
2021-04-17 20:02:16 -0500

    Add files via upload

---
+++ Ejemplo_003/LambdaTest.java
@@ -0,0 +1,9 @@
+public class LambdaTest{
+	public static void public static void main(String[] args) {
+		
+		//Expresión lambda ==> representa un ojeto de una interfaz funcional
+		Operaciones op = (num1, num2) System.out.printl(num1 + num2);
+
+		op.imprimeSuma(5,10);
+	}
+}
